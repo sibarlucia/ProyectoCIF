f4b1074f3e03d88bba645cfe217a402bb8c1ed8d
sibarlucia <87153157+sibarlucia@users.noreply.github.com>
2023-06-11 20:55:46 -0300

    Cambiando CORS config

--- BackCatalogo/server.js
+++ BackCatalogo/server.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose')
 
 app.use(cors())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
