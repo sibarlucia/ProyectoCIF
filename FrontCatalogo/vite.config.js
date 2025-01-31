3594162ab1df130edd4b009f77b262a9f3f80577
sibarlucia <87153157+sibarlucia@users.noreply.github.com>
2025-01-31 00:46:46 -0300

    update cif

--- FrontCatalogo/vite.config.js
+++ FrontCatalogo/vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Si la aplicación está en el dominio raíz
+  build: {
+    outDir:'dist',
+  },
+  server: {
+    hmr: false,
+  },
 
 })
 
