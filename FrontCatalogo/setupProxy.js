d5f62854a210871e39dc5a432b5d1709a2106cfe
sibarlucia <87153157+sibarlucia@users.noreply.github.com>
2025-01-31 21:54:30 -0300

    update back

--- FrontCatalogo/setupProxy.js
+++ FrontCatalogo/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/libros',
     createProxyMiddleware({
       target: 'https://181.85.164.67',
       changeOrigin: true,
