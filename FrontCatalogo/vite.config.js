c64eebd98a7e2168eb01c730f89b507b69d6d709
sibarlucia <87153157+sibarlucia@users.noreply.github.com>
2025-02-27 23:57:04 -0300

    jueves 27

--- FrontCatalogo/vite.config.js
+++ FrontCatalogo/vite.config.js
@@ -9,12 +9,14 @@ export default defineConfig({
     outDir: 'dist',
   },
   server: {
+    host: '0.0.0.0',  // Esto hace que Vite escuche en todas las interfaces de red
+    port: 5173,       // El puerto donde corre tu servidor
     proxy: {
       '/libros': 'http://localhost:3001', // Proxy para redirigir las solicitudes de '/libros' al backend
     },
     cors: true, // Habilitar CORS
     hmr: false, // Desactivar Hot Module Replacement si es necesario
-    port: 3000, // Puedes configurar el puerto si es necesario
+    //port: 3000, // Puedes configurar el puerto si es necesario
   },
 })
 
